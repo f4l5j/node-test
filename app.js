b7cf760258354377b54dcddffba5c5d3c7ccb237
f4l5j <78499844+f4l5j@users.noreply.github.com>
2023-11-19 12:33:59 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //RICHIESTA GET GESTITA DA JS
 
 const express = require('express');
-const { funzioneGet } = require('./file')
+const { funzioneGet } = require('./file.js')
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post('/dati', funzioneGet)
+app.get('/dati', funzioneGet)
 
 app.listen(port, () => {
     console.log(`Server Express in ascolto sulla porta ${port}`);
