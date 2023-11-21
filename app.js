fcc4086c5bd1ee547520ea89a7a9de163e784382
f4l5j <78499844+f4l5j@users.noreply.github.com>
2023-11-21 22:35:31 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-//RICHIESTA GET GESTITA DA JS
+'use strict';
 
 const express = require('express');
-const { funzioneGet } = require('./file.js')
 const app = express();
 const port = 3000;
 
+const { modificaDati } = require('./script.js')
+
 app.use(express.json());
 
-app.get('/dati', funzioneGet)
+app.get('/dati', (req, res) => {
+    const responseData = { message: 'CIAO MONDO' };
+
+    res.json(responseData);
+});
+
+app.post('/modify', (req, res) => {
+    res.json('CIAO ' + req.body.name);
+})
+
+app.post('/modifyScript', modificaDati)
 
 app.listen(port, () => {
-    console.log(`Server Express in ascolto sulla porta ${port}`);
+    console.log('Server in ascolto')
 })
