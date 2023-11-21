9b34d818976df59b8fec3edec659df1d42a1b7f8
f4l5j <78499844+f4l5j@users.noreply.github.com>
2023-11-21 22:38:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function modificaDati(req, res) {
+    if (!req.body || !req.body.message) {
+        return res.status(400).json({ error: 'Messaggio mancante nel corpo della richiesta.' });
+    }
+    const response = { message: 'ciao'+req.body.name }
+    res.json(response)
+}
+module.exports = { funzioneGet }
