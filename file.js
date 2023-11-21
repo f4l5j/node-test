3feb0cf8f9d1eb99efe8f7d3bd75a2cfc968b758
f4l5j <78499844+f4l5j@users.noreply.github.com>
2023-11-21 22:39:00 +0100

    Delete file.js

--- file.js
+++
@@ -1,8 +0,0 @@
-function funzioneGet(req, res) {
-    if (!req.body || !req.body.message) {
-        return res.status(400).json({ error: 'Messaggio mancante nel corpo della richiesta.' });
-    }
-    const response = { message: 'ciao' }
-    res.json(response)
-}
-module.exports = { funzioneGet }
