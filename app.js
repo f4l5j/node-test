eb975ac0031637814c2c0c6089813ec8854b317e
f4l5j <78499844+f4l5j@users.noreply.github.com>
2023-11-22 18:42:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 'use strict';
 
-const express = require('express');
+import express, { json } from 'express';
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-const { modificaDati } = require('./script.js')
-
-app.use(express.json());
-
-app.get('/dati', (req, res) => {
-    const responseData = { message: 'CIAO MONDO' };
-
-    res.json(responseData);
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'zbeffore_test-candidati',
+    password: 'z@w3%5TK{6*_',
+    database: 'zbeffore_test-candidati'
 });
 
-app.post('/modify', (req, res) => {
-    res.json('CIAO ' + req.body.name);
+
+// Connessione al database
+connection.connect((err) => {
+    if (err) {
+        console.error('Errore durante la connessione al database: ', err);
+        return;
+    }
+    console.log('Connessione al database riuscita');
+});
+
+app.use(json());
+
+app.get('/dati', (req, res) => {
+    connection.query('SELECT * FROM etichette', (error, results) => {
+        if (error) {
+            console.error('Errore durante la query al database:', error);
+            return;
+        }
+        res.json(results);
+        connection.end();
+    });
 })
 
-app.post('/modifyScript', modificaDati)
 
 app.listen(port, () => {
     console.log('Server in ascolto')
