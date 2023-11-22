40327db074a931628e9ca3088fd13e33cee3be74
f4l5j <78499844+f4l5j@users.noreply.github.com>
2023-11-22 21:37:48 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function modificaDati(req, res) {
-    if (!req.body || !req.body.message) {
-        return res.status(400).json({ error: 'Messaggio mancante nel corpo della richiesta.' });
-    }
-    const response = { message: 'ciao' + req.body.name }
-    res.json(response)
-}
-module.exports = { modificaDati }
