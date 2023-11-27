2f52afb7e035daf7f49ec4ec7d2f2a435e3ee952
f4l5j <78499844+f4l5j@users.noreply.github.com>
2023-11-27 23:39:02 +0100

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -13,18 +13,19 @@ const port = 3000;
 import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
-    host: 'mysql-ce2fb19-testnodejs.a.aivencloud.com',
-    port: '20440',
-    user: 'avnadmin',
-    ssl: {
-        ca: 'MIIEQTCCAqmgAwIBAgIUMbRwXhaPc0rVzU4GgrLvzvUUkecwDQYJKoZIhvcNAQEMBQAwOjE4MDYGA1UEAwwvMzMxNmJkNmItMmRlYy00MmQxLWE0NmEtZTg5ODU4MWYyNDUwIFByb2plY3QgQ0EwHhcNMjMxMTI3MjEyMzAxWhcNMzMxMTI0MjEyMzAxWjA6MTgwNgYDVQQDDC8zMzE2YmQ2Yi0yZGVjLTQyZDEtYTQ2YS1lODk4NTgxZjI0NTAgUHJvamVjdCBDQTCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBAJjxBpXfFZBXhwxJoHVzsxx129AOYQ6NARS36XXz+SZrwyWC9EZK6So4ZRtyMyy1nObeqx8DbHhzt/2pr6bBRjEdIDfgb4nY95nnFXDocY4E+ksCpLLvu4ZxfTIJ3RgztG/xPadVJlDvWGJhbBkvnF3W4A/K0GOnLmiGgFxd4D7rvE4/hRFvUq6ein3Q0OQyKp3gMcmh9Mu71Ye4w3ntnXbztfk+CzrdOdpm5wvzzs+JlWG+y/wvS9tQmcy/VTs8zHp/DowI4MhojPgHkHFRCPlWG/mz326JIwGAonDgmLyWig5b36tWzmVTnVhYWmjH76miydKx02Gi6LdOJlrX4MhLF9CEQIGfDo6DP8pPLqgUpb1N9L8T1WLwOzfr9Qb8ICmWq5XJ5F02kPrmwV+vEsSy/KXUpIXHfTH6qcvRaTCN7ypYPq9ebnWH9HIjwy4hktUvsbxBp+ImOTD9JQe2I+aq4ZDcWrrWSURmWwVpibuW2e2/tNBZnDawpFYEq4QuCQIDAQABoz8wPTAdBgNVHQ4EFgQUPDDzW+iso0+vRTAK5wNdWmEs55owDwYDVR0TBAgwBgEB /wIBADALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQEMBQADggGBAHZE7kZ0nAwFSXALmv+6+EpezuNh9oIMRM66VIB/4qLMa3RxnUW1SKk/VNVkDOVffFmExrz8SVKEBBipZWib3kMUwNoexoHGR6W0IXMku2MWuAc2+Yc+GWtsr5pCfJRdNLnVubZNxdIZ5CsnzlXya4TusdcBA/04uLkWbIL8k+Waw9dKv/6JZRLvtsJRIcBj9vwP5YvnnhcMdJOt70SNY91ktYtXcGo9c2GUgeVm3FSyIgumidBFA8B8T4qgfkZRMjjh2HFuf+onC1z9WXyGEAJJbecLn95zhqCAG167uEpL3+ADr8iR9/PJEaB5RtBh8OLJtgs9Sqqw0IOdpj2kqgR/keOXfBqe8ZRW7EDAZf55LB3CYyK1fONaNteQteZonJzhYq7AjrJ0aTLjZbp3dq4tBsLFfOTKCbnJfcFTUo1FIDMVY1i8/NxvCjDvAxNBbtOQsWZZe/In6ICGOiHkcTgIPdvgJvZ6YKt2HYtQh81NhtH5H5R1IObtx3GkaoU2HQ=='
-    }
+    host: 'sql.freedb.tech',
+    user: 'freedb_testo',
+    password: '6R9YqVgVeXdP@*5',
+    database: 'freedb_testdfsvsdv'
 });
 
 connection.connect((err) => {
     if (err) {
         console.error('Errore durante la connessione al database:', err);
         return;
+    }
+    else{
+        console.log('Connessione effettuata con successo')
     }
 })
 
